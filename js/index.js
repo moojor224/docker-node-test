f94b192e4aeb3fee3506427f52bfcdba6bb67fdd
moojor224 <moojor224@gmail.com>
2024-12-17 10:09:55 -0600

    commit

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-import { exec } from "child-process"
+import { exec } from "node:child_process";
 
 console.log("Node process is running. Press Ctrl+C to stop.");
 
 setInterval(() => {
-    exec("chown -R git:git /var/lib/gitea/", (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
-        if (stdout.length > 0) console.log(`stdout: ${stdout}`);
-        if (stderr.length > 0) console.error(`stderr: ${stderr}`);
-    });
+    // exec("chown -R git:git /var/lib/gitea/", (error, stdout, stderr) => {
+    //     if (error) {
+    //         console.error(`exec error: ${error}`);
+    //         return;
+    //     }
+    //     if (stdout.length > 0) console.log(`stdout: ${stdout}`);
+    //     if (stderr.length > 0) console.error(`stderr: ${stderr}`);
+    // });
 }, 1000);
